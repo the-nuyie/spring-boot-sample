4ebfeacfd93e131a0bdc395e8be8e0c44f476cf7
Tinnakorn Siritanyapong <the.nuyie@gmail.com>
2023-05-25 12:00:05 +0700

    Utility contoller is created for another useful.

---
+++ src/main/java/com/example/springbootsample/controller/UtilityController.java
@@ -0,0 +1,11 @@
+package com.example.springbootsample.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample-api/v1/utility")
+public class UtilityController {
+
+
+}
